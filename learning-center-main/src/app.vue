4af2716a45648ecbd81f9fe153cb49896a2d7c7f
Sebastián Pinedo <u202414345@upc.edu.pe>
2026-05-06 09:21:00 -0500

    Delete learning-center-main directory

--- learning-center-main/src/app.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import Layout from "./shared/presentation/components/layout.vue";
-import {useI18n} from "vue-i18n";
-const { t } = useI18n();
-</script>
-
-<template>
-  <layout/>
-</template>
